45820b6ddd301a52a5a2aa62d491b61d2081639d
cyq <caoyongqi@5i5j.com>
2025-02-11 18:05:39 +0800

    init

--- src/api/base/statistics.ts
+++ src/api/base/statistics.ts
@@ -2,7 +2,21 @@ import { IObjGet, IResponse } from '@/api';
 import { request } from '@@/plugin-request';
 
 export const fetchWeekData = async (options?: IObjGet) => {
-  return request<IResponse<any[]>>('/api/statistics/new/week', {
+  return request<IResponse<any>>('/api/statistics/new/week', {
+    method: 'GET',
+    ...(options || {}),
+  });
+};
+
+export const fetchWeekTaskData = async (options?: IObjGet) => {
+  return request<IResponse<any>>('/api/statistics/task/week', {
+    method: 'GET',
+    ...(options || {}),
+  });
+};
+
+export const fetchCurrentTaskData = async (options?: IObjGet) => {
+  return request<IResponse<any>>('/api/statistics/task/today', {
     method: 'GET',
     ...(options || {}),
   });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import { IProject } from '@/api';
 import { queryProject } from '@/api/base';
-import { fetchWeekData } from '@/api/base/statistics';
+import {
+  fetchCurrentTaskData,
+  fetchWeekData,
+  fetchWeekTaskData,
+} from '@/api/base/statistics';
 import { useModel } from '@@/exports';
+import { Line, Pie } from '@ant-design/plots';
 import { ProCard, StatisticCard } from '@ant-design/pro-components';
+import { Divider, Typography } from 'antd';
 import RcResizeObserver from 'rc-resize-observer';
 import { useEffect, useState } from 'react';
+
 const { Statistic } = StatisticCard;
+const { Text, Title } = Typography;
 
 interface IStatisticsWeek {
   apis: number;
@@ -14,57 +22,116 @@ interface IStatisticsWeek {
   apis_growth: number;
   uis: number;
   uis_growth: number;
+  ui_task: number;
   ui_task_growth: number;
 }
 
 export default function IndexPage() {
   const { initialState, setInitialState } = useModel('@@initialState');
   const [projectList, setProjectList] = useState<IProject[]>([]);
-  const [weekData, setWeekData] = useState<IStatisticsWeek>([]);
+  const [weekData, setWeekData] = useState<IStatisticsWeek>({
+    apis: 0,
+    api_task: 0,
+    api_task_growth: 0,
+    apis_growth: 0,
+    uis: 0,
+    uis_growth: 0,
+    ui_task: 0,
+    ui_task_growth: 0,
+  });
+  const [apiWeekTaskData, setApIWeekTaskData] = useState<any[]>([]);
+  const [uiWeekTaskData, setUIWeekTaskData] = useState<any[]>([]);
+  const [currentApiTaskData, setCurrentApiTaskData] = useState<any>();
+  const [currentUITaskData, setCurrentUITaskData] = useState<any>();
   const [responsive, setResponsive] = useState(false);
 
   useEffect(() => {
     queryProject().then(({ code, data }) => {
       if (code === 0) {
-        setProjectList([...data, ...data]);
+        setProjectList(data);
+      }
+    });
+
+    fetchCurrentTaskData().then(async ({ code, data }) => {
+      if (code === 0) {
+        setCurrentApiTaskData(data.api_task);
+        setCurrentUITaskData(data.ui_task);
       }
     });
     fetchWeekData().then(({ code, data }) => {
       if (code === 0) {
-        setWeekData(data);
+        setWeekData(data as IStatisticsWeek);
+      }
+    });
+    fetchWeekTaskData().then(({ code, data }) => {
+      if (code === 0) {
+        setApIWeekTaskData(data.api_tasks);
+        setUIWeekTaskData(data.ui_tasks);
       }
     });
   }, []);
+  console.log('===', currentApiTaskData);
+  const configAPI = {
+    data: apiWeekTaskData,
+    xField: 'date',
+    yField: 'num',
+    seriesField: 'name',
+    legend: {
+      position: 'top',
+    },
+    smooth: true,
+    animation: {
+      appear: {
+        animation: 'path-in',
+        duration: 5000,
+      },
+    },
+  };
+  const configUI = {
+    data: uiWeekTaskData,
+    xField: 'date',
+    yField: 'num',
+    seriesField: 'name',
+    legend: {
+      position: 'top',
+    },
+    smooth: true,
+    animation: {
+      appear: {
+        animation: 'path-in',
+        duration: 5000,
+      },
+    },
+  };
+  const PieAPIData = [
+    {
+      type: '成功',
+      value: currentApiTaskData?.success_num,
+    },
+    {
+      type: '失败',
+      value: currentApiTaskData?.fail_num,
+    },
+  ];
+  const ApiPieConfig = {
+    height: 50,
+    appendPadding: 2,
+    data: PieAPIData,
+    angleField: 'value',
+    colorField: 'type',
+    radius: 0.8,
+    label: {
+      type: 'outer',
+    },
+    interactions: [
+      {
+        type: 'element-active',
+      },
+    ],
+  };
 
   return (
     <ProCard split={'horizontal'}>
-      <ProCard>
-        <h1>welcome {initialState?.currentUser?.username}</h1>
-      </ProCard>
-      <ProCard split={'vertical'}>
-        {projectList.map((item) => {
-          return (
-            <StatisticCard
-              statistic={{
-                title: item.title,
-                value: 1,
-                icon: (
-                  <img
-                    style={{
-                      display: 'block',
-                      width: 42,
-                      height: 42,
-                      borderRadius: 4,
-                    }}
-                    src="https://gw.alipayobjects.com/mdn/rms_7bc6d8/afts/img/A*dr_0RKvVzVwAAAAAAAAAAABkARQnAQ"
-                    alt="icon"
-                  />
-                ),
-              }}
-            />
-          );
-        })}
-      </ProCard>
       <RcResizeObserver
         key="resize-observer"
         onResize={(offset) => {
@@ -83,7 +150,7 @@ export default function IndexPage() {
           >
             <StatisticCard
               statistic={{
-                title: 'API Case',
+                title: <Title level={5}>{'API Case'}</Title>,
                 value: weekData.apis,
                 description: (
                   <Statistic
@@ -96,7 +163,7 @@ export default function IndexPage() {
             />
             <StatisticCard
               statistic={{
-                title: 'UI Task',
+                title: <Title level={5}>{'API Task'}</Title>,
                 value: weekData.api_task,
                 description: (
                   <Statistic
@@ -109,7 +176,7 @@ export default function IndexPage() {
             />
             <StatisticCard
               statistic={{
-                title: 'UI Case',
+                title: <Title level={5}>{'UI Case'}</Title>,
                 value: weekData.uis,
                 description: (
                   <Statistic
@@ -122,8 +189,8 @@ export default function IndexPage() {
             />
             <StatisticCard
               statistic={{
-                title: 'UI Task',
-                value: weekData.api_task,
+                title: <Title level={5}>{'UI Task'}</Title>,
+                value: weekData.ui_task,
                 description: (
                   <Statistic
                     title="周同比"
@@ -134,6 +201,84 @@ export default function IndexPage() {
               }}
             />
           </StatisticCard.Group>
+        </ProCard>
+        <ProCard split={'vertical'} style={{ borderRadius: 10, marginTop: 20 }}>
+          <ProCard title={'API Task 构建情况'}>
+            <StatisticCard.Group
+              direction={responsive ? 'column' : 'row'}
+              title={'今日构建'}
+            >
+              <StatisticCard
+                statistic={{
+                  title: '总共',
+                  valueStyle: { color: 'greenyellow' },
+                  value: currentApiTaskData?.total_num,
+                  description: <Statistic value={currentApiTaskData?.date} />,
+                }}
+              />
+              <Divider type={responsive ? 'horizontal' : 'vertical'} />
+              <StatisticCard
+                statistic={{
+                  title: '成功',
+                  value: currentApiTaskData?.success_num,
+                  valueStyle: { color: 'green' },
+                  description: (
+                    <Statistic
+                      title="占比"
+                      value={
+                        currentApiTaskData?.success_num === 0
+                          ? '0%'
+                          : `${(
+                              (currentApiTaskData?.success_num /
+                                currentApiTaskData?.total_num) *
+                              100
+                            ).toFixed(2)}%`
+                      }
+                    />
+                  ),
+                }}
+                chartPlacement="left"
+              />
+              <StatisticCard
+                statistic={{
+                  title: '失败',
+                  value: currentApiTaskData?.fail_num,
+                  valueStyle: { color: 'red' },
+                  description: (
+                    <Statistic
+                      title="占比"
+                      value={
+                        currentApiTaskData?.fail_num === 0
+                          ? '0%'
+                          : `${(
+                              (currentApiTaskData?.fail_num /
+                                currentApiTaskData?.total_num) *
+                              100
+                            ).toFixed(2)}%`
+                      }
+                    />
+                  ),
+                }}
+                chartPlacement="left"
+              />
+              <Divider type={responsive ? 'horizontal' : 'vertical'} />
+              <StatisticCard chart={<Pie {...ApiPieConfig} />} />
+            </StatisticCard.Group>
+            <StatisticCard.Group
+              direction={'row'}
+              style={{ marginTop: 50 }}
+              // @ts-ignore
+              chart={<Line {...configAPI} />}
+            />
+          </ProCard>
+          <ProCard title={'UI Task 构建情况'}>
+            <StatisticCard.Group
+              direction={'row'}
+              style={{ marginTop: 50 }}
+              // @ts-ignore
+              chart={<Line {...configUI} />}
+            />
+          </ProCard>
         </ProCard>
       </RcResizeObserver>
     </ProCard>

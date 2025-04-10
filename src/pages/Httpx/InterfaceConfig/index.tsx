e0e1eaf231fa585ad84572d188aa9678de78cebe
cyq <caoyongqi@5i5j.com>
2025-04-10 14:11:24 +0800

    优化

--- src/pages/Httpx/InterfaceConfig/index.tsx
+++ src/pages/Httpx/InterfaceConfig/index.tsx
@@ -1,27 +1,21 @@
+import MyTabs from '@/components/MyTabs';
 import InterApiHeaders from '@/pages/Httpx/InterfaceConfig/InterApiHeaders';
 import InterApiVariables from '@/pages/Httpx/InterfaceConfig/InterApiVariables';
-import { ProCard } from '@ant-design/pro-components';
-import { Tabs } from 'antd';
 
 const Index = () => {
-  return (
-    <ProCard>
-      <Tabs
-        tabPosition={'top'}
-        type={'card'}
-        defaultValue={'1'}
-        size={'large'}
-        style={{ marginTop: '20px' }}
-      >
-        <Tabs.TabPane tab={'全局变量'} key={'1'}>
-          <InterApiVariables />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab={'默认请求头'} key={'2'}>
-          <InterApiHeaders />
-        </Tabs.TabPane>
-      </Tabs>
-    </ProCard>
-  );
+  const items = [
+    {
+      key: '1',
+      label: `全局变量`,
+      children: <InterApiVariables />,
+    },
+    {
+      key: '2',
+      label: `默认请求头`,
+      children: <InterApiHeaders />,
+    },
+  ];
+  return <MyTabs tabPosition={'top'} defaultActiveKey={'1'} items={items} />;
 };
 
 export default Index;

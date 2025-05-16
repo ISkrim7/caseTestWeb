181d9696aae323220ecf953bd6b6e13f125eab46
cyq <caoyongqi@5i5j.com>
2025-05-16 17:28:01 +0800

    变量优化

--- src/pages/Httpx/Interface/InterfaceApiDetail/ApiAfterItems.tsx
+++ src/pages/Httpx/Interface/InterfaceApiDetail/ApiAfterItems.tsx
@@ -1,7 +1,7 @@
 import MyTabs from '@/components/MyTabs';
 import InterAfterScript from '@/pages/Httpx/componets/InterAfterScript';
 import { IInterfaceAPI } from '@/pages/Httpx/types';
-import { SettingOutlined } from '@ant-design/icons';
+import { EditOutlined, FieldTimeOutlined } from '@ant-design/icons';
 import { ProCard } from '@ant-design/pro-components';
 import { FormInstance, TabsProps, Tooltip } from 'antd';
 import { FC } from 'react';
@@ -17,9 +17,15 @@ const ApiAfterItems: FC<SelfProps> = (props) => {
   const AfterItems: TabsProps['items'] = [
     {
       key: '1',
-      label: <Tooltip title="依次执行 设置变量、脚本、SQL">后操作</Tooltip>,
-      icon: <SettingOutlined />,
+      label: <Tooltip title="依次执行 设置变量、脚本、SQL">后置脚本</Tooltip>,
+      icon: <EditOutlined />,
       children: <InterAfterScript form={interApiForm} mode={currentMode} />,
+    },
+    {
+      key: '2',
+      label: <Tooltip title="只适用于业务场景中,调试不生效">等待</Tooltip>,
+      icon: <FieldTimeOutlined />,
+      children: null,
     },
   ];
   return (

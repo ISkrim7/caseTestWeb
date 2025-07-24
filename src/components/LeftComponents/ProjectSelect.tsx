5e050b957c74ca3dae50312494162bb93c6a954d
hinn <570096125@qq.com>
2025-07-24 18:09:17 +0800

    选择项目优化

--- src/components/LeftComponents/ProjectSelect.tsx
+++ src/components/LeftComponents/ProjectSelect.tsx
@@ -1,8 +1,8 @@
 import { IProject } from '@/api';
 import { data2LabelValue } from '@/utils/somefunc';
-import { ProjectTwoTone } from '@ant-design/icons';
+import { CloseOutlined, ProjectTwoTone } from '@ant-design/icons';
 import { ProCard } from '@ant-design/pro-components';
-import { Select, Typography } from 'antd';
+import { Button, Select, Typography } from 'antd';
 import React, { FC } from 'react';
 
 const { Title } = Typography;
@@ -22,17 +22,27 @@ const ProjectSelect: FC<IProps> = ({
     <>
       {currentProjectId ? (
         <ProCard ghost={true} bodyStyle={{ padding: 0, marginTop: 15 }}>
-          <Title
-            level={3}
-            onClick={() => setCurrentProjectId(undefined)}
-            style={{
-              marginLeft: 12,
-              marginBottom: 20,
-            }}
-          >
-            <ProjectTwoTone style={{ marginRight: 10 }} />
-            {projects.find((item) => item.id === currentProjectId)?.title}
-          </Title>
+          <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
+            <Title
+              level={3}
+              style={{
+                marginLeft: 12,
+                marginBottom: 20,
+                marginRight: 8,
+              }}
+            >
+              <ProjectTwoTone style={{ marginRight: 10 }} />
+              {projects.find((item) => item.id === currentProjectId)?.title}
+            </Title>
+            <Button
+              type="text"
+              icon={<CloseOutlined />}
+              onClick={() => setCurrentProjectId(undefined)}
+              style={{ color: '#ff4d4f' }}
+            >
+              选择项目
+            </Button>
+          </div>
         </ProCard>
       ) : (
         <Select
